54d2ac018ce1c6be2c2e87bbfb3b85c9a42eaee3
eyoo95 <105832377+eyoo95@users.noreply.github.com>
2022-05-24 11:19:50 +0900

    add

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+    # 파이썬에서 가상환경을 확인한다.
+    # 이앱은 구글 코랩에서 만든 아보카도 가격예측 환경과 동일하게 하기 뤼해서 가상환경을 python 3.7로 바꿔준다.
+    # 파이썬 가상환경을 streamlit3.7로 바꿔준다
